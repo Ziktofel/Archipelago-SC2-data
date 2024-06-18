511d853c33e86369901cd604dbf1f64915a3d828
Ziktofel <ziktofel@gmail.com>
2024-06-18 23:17:08 +0200

    Minor fixes to schema validation

--- scripts/setup_xml_schema_validation.sh
+++ scripts/setup_xml_schema_validation.sh
@@ -3,6 +3,9 @@ set -e
 
 script_dir=$PWD
 cd "$script_dir/.."
-find . -name "UpgradeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/SchemaUpgradeData.xsd">#g' {} \;
-find . -name "RequirementData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/SchemaRequirementData.xsd">#g' {} \;
-find . -name "RequirementNodeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/RequirementNodeData.xsd">#g' {} \;
+find Mods -name "UpgradeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/SchemaUpgradeData.xsd">#g' {} \;
+find Maps -name "UpgradeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../../../schemas/SchemaUpgradeData.xsd">#g' {} \;
+find Mods -name "RequirementData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/SchemaRequirementData.xsd">#g' {} \;
+find Maps -name "RequirementData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../../../schemas/SchemaRequirementData.xsd">#g' {} \;
+find Mods -name "RequirementNodeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../schemas/SchemaRequirementNodeData.xsd">#g' {} \;
+find Maps -name "RequirementNodeData.xml" -exec sed -Ei 's#^<Catalog>#<Catalog xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../../../../../schemas/SchemaRequirementNodeData.xsd">#g' {} \;
